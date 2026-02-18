b211f290631579456d19f6fdb40a52a53414bf68
whattis503 <whattis503@users.noreply.github.com>
2026-02-18 10:14:28 +0000

    Deploy: Drag handle only for reorder, permanent calendar/API tokens

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1857605807"
+    serviceWorkerVersion: "3182740922"
   }
 });
