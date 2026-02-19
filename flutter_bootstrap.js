d9902766107f2a95bea61fa6a132e98e80283a3c
whattis503 <genspark_flutter_dev@genspark.ai>
2026-02-19 14:35:25 +0000

    Deploy: Todo copy feature, actual time sum display (fresh build)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4163621792"
+    serviceWorkerVersion: "1211860711"
   }
 });
